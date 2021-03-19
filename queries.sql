2239696634929fb88fbb9d8de1aa231df1fff5a4
Sam-Berger <sam.d.berger@gmail.com>
2021-03-19 11:17:28 -0500

    Completed Part 2, added employer and skill controller and assignment query 2

--- queries.sql
+++ queries.sql
@@ -1,11 +1,15 @@
 ## Part 1: Test it with SQL
 
---id: INT (this is also the primary key)
---employer VARCHAR
---name VARCHAR
---skills VARCHAR
+id: INT (this is also the primary key)
+employer VARCHAR
+name VARCHAR
+skills VARCHAR
 
 ## Part 2: Test it with SQL
+
+SELECT name
+FROM techjobs.employer
+WHERE (location="St. Louis City")
 
 ## Part 3: Test it with SQL
 
